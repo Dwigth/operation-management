a44ed63a6c830a5f36cc0673be5454b3aee5ead8
Dwigth Astacio <dwigth32@outlook.es>
2023-01-31 16:37:31 -0600

    feat: adding auth endpoints

--- apps/operation-backend/src/app/app.controller.ts
+++ apps/operation-backend/src/app/app.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Body } from '@nestjs/common';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
+import { SignupDTO } from '@operation-management/common';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
-import { AppService } from './app.service';
-
-@Controller()
+@ApiTags('Auth')
+@ApiHeader({
+  name: 'x-travel-token',
+  description: 'Token for auth',
+  required: true,
+  allowEmptyValue: false,
+})
+@Controller({
+  version: '1',
+})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+    ) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req: Request & { user: any}) {
+    return this.authService.login(req.user);
   }
+
+  @Post('auth/signup')
+  async signup(@Body() data: SignupDTO) {
+    return this.authService.signup(data);
+  }
+
 }
