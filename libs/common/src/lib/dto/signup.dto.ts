730523c0b3c01dfd8f7df522e650c9ca90d889b3
Dwigth Astacio <dwigth32@outlook.es>
2023-01-31 18:12:34 -0600

    feat: adding decorators for swagger

--- libs/common/src/lib/dto/signup.dto.ts
+++ libs/common/src/lib/dto/signup.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SignupDTO {
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     password: string;
 }
 
