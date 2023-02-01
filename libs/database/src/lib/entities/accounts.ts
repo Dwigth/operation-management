1205b09d3fa10beef7b062f275b9eca5f7717ac2
Dwigth Astacio <dwigth32@outlook.es>
2023-02-01 15:23:53 -0600

    feat: added update and delete special columns to entity & migration

--- libs/database/src/lib/entities/accounts.ts
+++ libs/database/src/lib/entities/accounts.ts
@@ -1,34 +1,36 @@
 import {
   Column,
+  DeleteDateColumn,
   Entity,
   Index,
   OneToMany,
   PrimaryGeneratedColumn,
-} from "typeorm";
-import { AccountTeams } from "./account-teams";
+  UpdateDateColumn,
+} from 'typeorm';
+import { AccountTeams } from './account-teams';
 
-@Index("accounts_pkey", ["id"], { unique: true })
-@Entity("accounts", { schema: "public" })
+@Index('accounts_pkey', ['id'], { unique: true })
+@Entity('accounts', { schema: 'public' })
 export class Accounts {
-  @PrimaryGeneratedColumn({ type: "integer", name: "id" })
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
   id: number;
 
-  @Column("character varying", {
-    name: "account_name",
+  @Column('character varying', {
+    name: 'account_name',
     nullable: true,
     length: 100,
   })
   accountName: string | null;
 
-  @Column("character varying", {
-    name: "client_name",
+  @Column('character varying', {
+    name: 'client_name',
     nullable: true,
     length: 100,
   })
   clientName: string | null;
 
-  @Column("character varying", {
-    name: "operation_manager_name",
+  @Column('character varying', {
+    name: 'operation_manager_name',
     nullable: true,
     length: 100,
   })
@@ -36,4 +38,26 @@ export class Accounts {
 
   @OneToMany(() => AccountTeams, (accountTeams) => accountTeams.account)
   accountTeams: AccountTeams[];
+
+  @UpdateDateColumn({
+    name:'updated_date'
+  })
+  updated: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_date'
+  })
+  deletedAt: Date;
+
+  public setAccountName(accountName: string): void {
+    if (accountName && accountName != '') this.accountName = accountName;
+  }
+
+  public setClientName(clientName: string): void {
+    if (clientName && clientName != '') this.clientName = clientName;
+  }
+
+  public setOperationManagerName(operationManagerName: string): void {
+    if (operationManagerName && operationManagerName != '') this.operationManagerName = operationManagerName;
+  }
 }
---
+++ libs/database/src/lib/migrations/9_update-and-delete-column-in-accounts-table.sql
@@ -0,0 +1,9 @@
+ALTER TABLE
+    public."accounts"
+ADD
+    updated_date date NULL;
+
+ALTER TABLE
+    public."accounts"
+ADD
+    deleted_date date NULL;
