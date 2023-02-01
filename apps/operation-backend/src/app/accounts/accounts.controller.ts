e3a7d4192d07e7b386e0471b2648e6936715e8f8
Dwigth Astacio <dwigth32@outlook.es>
2023-02-01 15:26:29 -0600

    feat: accounts crud endpoints

--- apps/operation-backend/src/app/accounts/accounts.controller.ts
+++ apps/operation-backend/src/app/accounts/accounts.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, UseGuards, Request, Body, Get, Put, Delete } from '@nestjs/common';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ROLES, SWAGGER } from '@operation-management/common';
+import { Controller, Post, UseGuards, Request, Body, Get, Put, Delete, Query } from '@nestjs/common';
+import { ApiBody, ApiHeader, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateAccountDto, ListQuery, ROLES, SWAGGER, UpdateAccountDto } from '@operation-management/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Roles } from '../auth/roles.decorator';
 import { RolesGuard } from '../auth/roles.guard';
 import { AccountsService } from './accounts.service';
 
-const { AUTH, ERRORS } = SWAGGER;
+const { ACCOUNTS, ERRORS } = SWAGGER;
 
 @ApiTags('Accounts')
+@ApiHeader({
+    name: 'x-operations-key',
+    description: 'Token for auth',
+    required: true,
+    allowEmptyValue: false,
+  })
 @Controller({
   version: '1',
   path: 'accounts'
@@ -22,48 +28,47 @@ export class AccountsController {
   @Roles(ROLES.admin, ROLES.super_user)
   @Post('')
   @ApiResponse(ERRORS.ForbiddenResource)
-  async create() { 
-    //
+  @ApiResponse(ACCOUNTS.RESPONSES.CREATE)
+  async create(@Body() accountDto: CreateAccountDto) { 
+    return await this.accountsService.createOne(accountDto);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(ROLES.admin, ROLES.super_user)
   @Get('')
   @ApiResponse(ERRORS.ForbiddenResource)
-  async get() { 
-    //
+  @ApiResponse(ERRORS.NotFound)
+  @ApiResponse(ACCOUNTS.RESPONSES.GET_ONE)
+  async get(@Query('accountId') accountId: number) { 
+    return await this.accountsService.getOne(accountId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(ROLES.admin, ROLES.super_user)
   @Put('')
   @ApiResponse(ERRORS.ForbiddenResource)
-  async update() { 
-    //
+  @ApiResponse(ERRORS.NotFound)
+  @ApiResponse(ACCOUNTS.RESPONSES.GET_ONE)
+  async update(@Body() updateDto: UpdateAccountDto) { 
+    return await this.accountsService.updateOne(updateDto);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(ROLES.admin, ROLES.super_user)
   @Delete('')
   @ApiResponse(ERRORS.ForbiddenResource)
-  async delete() { 
-    //
+  async delete(@Query('accountId') accountId: number) { 
+    return await this.accountsService.deleteOne(accountId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(ROLES.admin, ROLES.super_user)
   @Get('list')
   @ApiResponse(ERRORS.ForbiddenResource)
-  async list() { 
-    //
-  }
-
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(ROLES.admin, ROLES.super_user)
-  @Get('members')
-  @ApiResponse(ERRORS.ForbiddenResource)
-  async getMembers() { 
-    //
+  @ApiResponse(ERRORS.NotAuthorized)
+  @ApiResponse(ACCOUNTS.RESPONSES.GET_MANY)
+  async list(@Query() dto: ListQuery) { 
+    return await this.accountsService.list(dto);
   }
 
 }
--- apps/operation-backend/src/app/accounts/accounts.service.ts
+++ apps/operation-backend/src/app/accounts/accounts.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  AccountRetrieveDto,
+  CreateAccountDto,
+  ListQuery,
+  UpdateAccountDto,
+} from '@operation-management/common';
+import { Accounts } from '@operation-management/database';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
+import { Logger } from 'winston';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  constructor(
+    @InjectRepository(Accounts)
+    private accountsRepo: Repository<Accounts>,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
+  ) {}
+
+  async createOne({
+    accountName,
+    clientName,
+    operationManagerName,
+  }: CreateAccountDto) {
+    //
+    const account = await this.accountsRepo.save({
+      accountName,
+      clientName,
+      operationManagerName,
+    });
+
+    return {
+      id: account.id,
+      created: new Date(),
+    };
+  }
+
+  async getOne(accountId: number) {
+    const account = await this.accountsRepo.findOneBy({
+      id: accountId,
+    });
+    if (!account) {
+      this.logger.info(
+        `[${UsersService.name}] ACCOUNT WITH ID ${accountId} NOT FOUND`
+      );
+      throw new NotFoundException();
+    }
+    return account;
+  }
+
+  async updateOne({
+    id,
+    accountName,
+    clientName,
+    operationManagerName,
+  }: UpdateAccountDto): Promise<Accounts | undefined> {
+    const dbAccount = await this.accountsRepo.findOneBy({ id });
+
+    if (!dbAccount) {
+      this.logger.info(
+        `[${UsersService.name}] ACCOUNT WITH ID ${id} NOT FOUND`
+      );
+      throw new NotFoundException();
+    }
+    dbAccount.setAccountName(accountName);
+    dbAccount.setClientName(clientName);
+    dbAccount.setOperationManagerName(operationManagerName);
+
+    return await this.accountsRepo.save(dbAccount);
+  }
+
+  async deleteOne(id: number) {
+    const { affected } = await this.accountsRepo.softDelete(id);
+    if (affected > 0) {
+      return {
+        message: 'Successfully deleted',
+        deleted: new Date(),
+      };
+    }
+    return {
+      message: 'Could not deleted',
+      deleted: null,
+    };
+  }
+
+  async list({ skip, take }: ListQuery): Promise<AccountRetrieveDto[]> {
+    return await this.accountsRepo.find({
+      skip,
+      take,
+      select: ['id', 'accountName', 'clientName', 'operationManagerName'],
+      order: { id: 'ASC' },
+    });
+  }
+}
