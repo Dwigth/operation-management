f37a32fb55978d4823fc553d9ac004c7f62c7271
Dwigth Astacio <dwigth32@outlook.es>
2023-02-05 21:29:42 -0600

    feat: menu module

---
+++ apps/operation-app/src/app/dashboard/components/menu/menu-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+
+interface MenuItem {
+    label: string;
+    icon: string;
+    page: string;
+}
+
+@Component({
+    selector: 'operation-management-menu-item',
+    template: `
+    <a class="nav-link" [routerLink]="item.page">
+      <span class="nav-link-icon d-md-none d-lg-inline-block">
+             <i [class]="'ti ' + item.icon"></i>
+      </span>
+      <span class="nav-link-title">
+            {{item.label}}
+        </span>
+    </a>
+    `,
+})
+
+export class MenuItemComponent {
+    @Input() item: MenuItem;
+
+    constructor() {
+        //
+     }
+}
